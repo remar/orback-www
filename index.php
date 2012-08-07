147310a466a6a2a3ed28525727f19774d54ee2f4
Andreas Remar <andreas.remar@gmail.com>
2012-08-07 20:50:15 +0200

    Initial commit.

---
+++ index.php
@@ -0,0 +1,55 @@
+    <?php global $page; $page = 1; ?>
+
+    <?php include('head.html'); ?>
+
+    <div style="background-color: white;width:750px;border-style: solid;border-width:2px;padding: 0px 20px;background-image: url(images/djur_ljus.jpg);background-repeat:no-repeat;background-position: center">
+      <h1>Örbäck</h1>
+
+      En levande gård i samspel med natur, djur, odlingar, visioner om
+      ett annat sätt att leva på. Under sommaren har vi självplock av
+      de grönsaker vi odlar i vår köksträdgård och hemmabyggda
+      växthus. I gårdsbutiken finns honung, must och sylter vi gjort
+      från det som växer här. Kom och njut av en kaffe i vårt
+      utomhusfik. Varför inte klappa grisarna eller se lammen
+      busa. Vill ni stanna över och ”bo på lantgård” har vi två olika
+      boenden, beroende på vilken bekvämlighet som önskas, antingen
+      bor du i Sollyckan eller i Smeden. Vi ligger mellan Motala och
+      Linköping. Härifrån kan du med cykel ta dig till utflyktsmål som
+      Götakanal ...
+
+      <h2>Gårdsbutik</h2> 
+
+      I vår gårdsbutik säljer vi sådant som vi förädlat av det som
+      växer på gården. Vi gör sylter, chutney, honung, must mm.
+
+      <h2>Café</h2>
+
+      I sommar har vi ett utomhuscafé lördagar och söndagar. Sitt i
+      trädgården under ett äppelträd eller ta en filt och hitta en
+      plats i solen. Förutom kaffe och kakor har vi mackor och
+      sallader. I så stor utsträckning som möjligt kommer
+      ingredienserna från gårdsproduktionen.
+
+      <h2>Självplock</h2>
+      
+      Plocka av de grönsaker vi odlar. Majs, morötter, sockerärtor,
+      bondbönor, rödbetor mm.
+
+      <h2>Djur</h2> 
+
+      Vi har Linderögrisar, Highland Cattle kor, höns, och
+      Hälsingefår.
+
+      <h2>Boende</h2>
+      
+      <b>Sollyckan</b> - Rum med 4 bäddar. Eget kök, toalett, bastu
+      och dusch. Pris natt: Pris vecka: pris lakan:
+
+      <br/><br/>
+
+      <b>Smeden</b> - Rum med 2 bäddar, delat kök utedass och delad
+      dusch. Pris: natt Pris vecka: Pris lakan:
+
+    </div>
+  </body>
+</html>
---
+++ kontakt.php
@@ -0,0 +1,24 @@
+    <?php global $page; $page = 2; ?>
+
+    <?php include('head.html'); ?>
+
+    <div style="background-color: white;width:750px;border-style: solid;border-width:2px;padding: 0px 20px;background-repeat:no-repeat;background-position: center">
+      <h1>Kontakt</h1>
+      
+      <b>Petra</b><br/>
+      Tel: 0706 11 29 67<br/>
+      <br/>
+      Örbäck kultur<br/>
+      Örbäck 25<br/>
+      590 32 Klockrike<br/>
+      <br/>
+
+      <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=205185665512694972131.0004c6b1544b359361df4&amp;ie=UTF8&amp;t=m&amp;ll=58.525974,15.330048&amp;spn=0.062739,0.145912&amp;z=12&amp;output=embed"></iframe><br /><small>View <a href="https://maps.google.com/maps/ms?msa=0&amp;msid=205185665512694972131.0004c6b1544b359361df4&amp;ie=UTF8&amp;t=m&amp;ll=58.525974,15.330048&amp;spn=0.062739,0.145912&amp;z=12&amp;source=embed" style="color:#0000FF;text-align:left">Örbäck kultur</a> in a larger map</small>
+
+      <br/><br/>
+
+      <center><img src="images/djur.jpg" /></center>
+
+    </div>
+  </body>
+</html>
