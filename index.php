2f7a14fb1607e3dcaf11ade4940840f73077ac4d
Andreas Remar <andreas.remar@gmail.com>
2012-08-23 14:40:29 +0200

    Ändringar från Petra.

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <div style="background-color: white;width:750px;border-style: solid;border-width:2px;padding: 0px 20px;background-image: url(images/djur_ljus.jpg);background-repeat:no-repeat;background-position: center">
       <h1>Örbäck</h1>
 
-      En levande gård i samspel med natur, djur, odlingar, visioner om
-      ett annat sätt att leva på. Under sommaren har vi självplock av
-      de grönsaker vi odlar i vår köksträdgård och hemmabyggda
+      Är en levande gård i samspel med natur, djur, odlingar, visioner
+      om ett annat sätt att leva på. Under sommaren har vi självplock
+      av de grönsaker vi odlar i vår köksträdgård och hemmabyggda
       växthus. I gårdsbutiken finns honung, must och sylter vi gjort
       från det som växer här. Kom och njut av en kaffe i vårt
       utomhusfik. Varför inte klappa grisarna eller se lammen
@@ -24,10 +24,10 @@
 
       <h2>Café</h2>
 
-      I sommar har vi ett utomhuscafé lördagar och söndagar. Sitt i
-      trädgården under ett äppelträd eller ta en filt och hitta en
-      plats i solen. Förutom kaffe och kakor har vi mackor och
-      sallader. I så stor utsträckning som möjligt kommer
+      I sommar har vi ett utomhuscafé måndag till söndag
+      10.00-17.00. Sitt i trädgården under ett äppelträd eller ta en
+      filt och hitta en plats i solen. Förutom kaffe och kakor har vi
+      mackor och sallader. I så stor utsträckning som möjligt kommer
       ingredienserna från gårdsproduktionen.
 
       <h2>Självplock</h2>
@@ -43,11 +43,12 @@
       <h2>Boende</h2>
       
       <b>Sollyckan</b> - Rum med 4 bäddar. Eget kök, toalett, bastu
-      och dusch. Pris natt: Pris vecka: pris lakan:
+      och dusch. Pris natt: 500 kr Pris vecka: 3000 kr pris lakan: 50
+      kr. Det går att beställa frukost: 50 kr
 
       <br/><br/>
 
       <b>Smeden</b> - Rum med 2 bäddar, delat kök utedass och delad
-      dusch. Pris: natt Pris vecka: Pris lakan:
+      dusch. Pris natt: 200 kr Pris lakan: 50 kr, Frukost ingår.
 
       <?php include('tail.html') ?>
--- kontakt.php
+++ kontakt.php
@@ -4,14 +4,27 @@
 
     <div style="background-color: white;width:750px;border-style: solid;border-width:2px;padding: 0px 20px;background-repeat:no-repeat;background-position: center">
       <h1>Kontakt</h1>
-      
-      <b>Petra</b><br/>
-      Tel: 0706 11 29 67<br/>
-      <br/>
-      Örbäck kultur<br/>
-      Örbäck 25<br/>
-      590 32 Klockrike<br/>
-      <br/>
+
+      <table style="width:100%">
+	<tr>
+	  <td>
+	    <b>Axel Lindén</b><br/>
+	    Tel: 070 15 19 471<br/>
+	    E-post: <a href="mailto:axhel@spray.se">axhel@spray.se</a><br/>
+	    <br/>
+	    Örbäck kultur<br/>
+	    Örbäck 25<br/>
+	    590 32 Klockrike<br/>
+	  </td>
+	  <td>
+	    <center>
+	    <a href="http://www.facebook.com/orbackkultur"><img src="images/f_logo.png" align="middle"/><br/><br/>Besök oss på Facebook.</a>
+	    </center>
+	  </td>
+	</tr>
+      </table>
+
+      <br/><br/>
 
       <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=205185665512694972131.0004c6b1544b359361df4&amp;ie=UTF8&amp;t=m&amp;ll=58.525974,15.330048&amp;spn=0.062739,0.145912&amp;z=12&amp;output=embed"></iframe><br />
       <small>Visa <a href="https://maps.google.com/maps/ms?msa=0&amp;msid=205185665512694972131.0004c6b1544b359361df4&amp;ie=UTF8&amp;t=m&amp;ll=58.525974,15.330048&amp;spn=0.062739,0.145912&amp;z=12&amp;source=embed" style="color:#0000FF;text-align:left">Örbäck kultur</a> på en större karta</small>
